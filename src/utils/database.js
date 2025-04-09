22fe3244dad8b005ffe1304c90fb84ce87518105
Aakash <aakashsuresh455@gmail.com>
2025-04-09 21:41:16 +0530

    updated package

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const ConnectDB = async (req,res) => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI);
+    // const connect = await mongoose.connect(process.env.MONGO_URI);
+    const connect = await mongoose.connect('mongodb+srv://aakashakshith455:GqSx8UA5sqiJO9UC@cluster0.b6qrj.mongodb.net/DiamondTechnologies?retryWrites=true&w=majority&appName=Cluster0');
     console.log(
       "Database Connected : ",
       connect.connection.host,
