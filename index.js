bf9b150a30c4e27010b113fb27ee57cf4ba30c9a
Aakash <aakashsuresh455@gmail.com>
2025-04-10 18:29:22 +0530

    modified cors

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const cors = require('cors');
-
-// Configure CORS
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
